4298a230ffca922a0900f098c6742501806f8b55
rmaliwatu <rmaliwatu@cs.uct.ac.za>
2019-07-20 21:33:05 +0200

    completed first stab at profilesN in specResp

--- PAWSC_REST_API_RICHARD/django/base/src/PAWSCFunction.py
+++ PAWSC_REST_API_RICHARD/django/base/src/PAWSCFunction.py
@@ -27,23 +27,29 @@ class pawscFunction:
         """      
        
         data = UnassignedFreq.objects.all().values("freqstart", "freqend").filter(band = 900)
-        freq_ranges = []     
+        freq_ranges = []
+        freq_ranges_Hz = []
         for item in data: #UnassignedFreq.objects.all().values("freqstart", "freqend"):
             #print (item['freqstart'], item['freqend'])
             freq_ranges.append(FreqRangeToArfcnRange(freq_band,tech, item['freqstart'], item['freqend'], bw))
             #FreqRangeToArfcnRange(freq_band,tech, item['freqstart'], item['freqend'] , bw)
-         
+            freq_ranges_Hz.append({'freqstart': item['freqstart']*1000000, 'freqend': item['freqend']*1000000})
         #print (freq_ranges)
         #for item in UnassignedFreq.objects.all().values("freqstart", "freqend"):
             #print item
         #item = (880, 885)
-        #return (FreqRangeToArfcnRange('900E','GSM',item[0], item[1], 0.2) )  
-        print ('START ntwenu:')
-        for ntwenu in freq_ranges:
-            print (ntwenu['arfcn_start'])
-        print ('END ntwenu' )     
-        
-        return data, "profilesN", freq_ranges
+        #return (FreqRangeToArfcnRange('900E','GSM',item[0], item[1], 0.2) ) 
+        '''
+        Attempting to format the frequency ranges in a way client expects
+        '''
+        arfcn = []
+        for item in freq_ranges:
+            #print ("DARFCN:", item['arfcn_start']['arfcn_DL'], "UARFCN:", item['arfcn_start']['arfcn_UL'], "Ddbm:  30.0, Udbm: 20.0" )
+            arfcn.append({"DARFCN": item['arfcn_start']['arfcn_DL'], "UARFCN": item['arfcn_start']['arfcn_UL'], "Ddbm": 30.0, "Udbm": 20.0})
+            arfcn.append({"DARFCN": item['arfcn_end']['arfcn_DL'], "UARFCN": item['arfcn_end']['arfcn_UL'], "Ddbm": 30.0, "Udbm": 20.0})        
+              
+         
+        return freq_ranges_Hz, "profilesN", arfcn
         """
         May need to strip off the brackets depending on output format requirements
         """    
